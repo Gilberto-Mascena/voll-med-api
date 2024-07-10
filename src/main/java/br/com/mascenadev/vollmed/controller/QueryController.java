227f6d3a3b912a0a5a703ed61071041456253d8e
Gilberto Mascena <gilbertomascena@gmail.com>
2024-07-10 10:01:33 -0300

    created record QueryContrller

---
+++ src/main/java/br/com/mascenadev/vollmed/controller/QueryController.java
@@ -0,0 +1,24 @@
+package br.com.mascenadev.vollmed.controller;
+
+import br.com.mascenadev.vollmed.dto.DetailingConsultationDTO;
+import br.com.mascenadev.vollmed.dto.SchedulingConsultationDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consulta")
+public class QueryController {
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity toSchedule(@RequestBody @Valid SchedulingConsultationDTO schedulingConsultation) {
+
+        System.out.println("Scheduling consultation: " + schedulingConsultation);
+        return ResponseEntity.ok(new DetailingConsultationDTO(null, null, null, null)).build();
+    }
+}
