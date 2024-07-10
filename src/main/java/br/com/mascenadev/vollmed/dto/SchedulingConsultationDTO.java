a031a04a97efaea6826a9e0db504b48cbae3a089
Gilberto Mascena <gilbertomascena@gmail.com>
2024-07-10 10:03:25 -0300

    SchedulingConsultationDTO

---
+++ src/main/java/br/com/mascenadev/vollmed/dto/SchedulingConsultationDTO.java
@@ -0,0 +1,17 @@
+package br.com.mascenadev.vollmed.dto;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record SchedulingConsultationDTO(
+        Long idDoctor,
+
+        @NotNull
+        Long idPatient,
+
+        @NotNull
+        @Future
+        LocalDateTime date) {
+}
